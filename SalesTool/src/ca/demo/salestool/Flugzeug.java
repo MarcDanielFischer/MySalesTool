70663b57f72582dcf61448267f38260ce1718a52
Daniel Fischer <marc.daniel.fischer@gmail.com>
2015-03-02 17:18:01 +0100

    new Class Flugzeug

---
+++ SalesTool/src/ca/demo/salestool/Flugzeug.java
@@ -0,0 +1,13 @@
+package ca.demo.salestool;
+
+public class Flugzeug {
+	
+	public void abheben(){
+		System.out.println("Yippie, wir heben ab!");
+	}
+	
+	public void fliegen(){
+		System.out.println("Hurra, wir fliegen!");
+	}
+
+}
