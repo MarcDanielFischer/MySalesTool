559ac5919a6917b89a2241a0bf11fbe41285249b
Daniel Fischer <marc.daniel.fischer@gmail.com>
2015-03-02 13:01:37 +0100

    removed bad language

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -11,8 +11,8 @@ public class SalesData {
 
 		int sum = 0;
 		for (int i: data) {
-//			System.out.println("Next value: " + data[i]); // now thats the wrong index usage, you dumbass fuckheads!
-			System.out.println("Next value: " + i); // this is how real men with huge cocks do it
+//			System.out.println("Next value: " + data[i]); // now thats the wrong index usage
+			System.out.println("Next value: " + i); 
 			sum += i;
 		}
 		
